28825f87c60ea0e5c3c91fd9a8ba4a9bc779e2b6
Derek Singson <Derek Singson>
2017-04-05 18:17:34 -0400

    Added labels and controls for the View Inventory form!

---
+++ MarathonFinalProject/MarathonFinalProject/ViewInventory.Designer.vb
@@ -0,0 +1,363 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ViewInventory
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.chkboxWater = New System.Windows.Forms.CheckBox()
+        Me.chkboxSnack = New System.Windows.Forms.CheckBox()
+        Me.chkbox5K = New System.Windows.Forms.CheckBox()
+        Me.chkbox10K = New System.Windows.Forms.CheckBox()
+        Me.chkboxXShirt = New System.Windows.Forms.CheckBox()
+        Me.chkboxLargeShirt = New System.Windows.Forms.CheckBox()
+        Me.chkboxMedShirt = New System.Windows.Forms.CheckBox()
+        Me.chkboxSmallshirt = New System.Windows.Forms.CheckBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Water Bottle Count:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(72, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(105, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Snack Count:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(51, 173)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(126, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "5K Medal Count:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(42, 236)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(135, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "10K Medal Count:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(398, 107)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(163, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Medium Medal Count:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(425, 49)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(136, 20)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Small Shirt Count:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(413, 170)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(148, 20)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Large Medal Count:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(397, 233)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(164, 20)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "X-Large Medal Count:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(183, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(73, 26)
+        Me.TextBox1.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(183, 110)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(73, 26)
+        Me.TextBox2.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(183, 170)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(73, 26)
+        Me.TextBox3.TabIndex = 10
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Location = New System.Drawing.Point(183, 233)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(73, 26)
+        Me.TextBox4.TabIndex = 11
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Enabled = False
+        Me.TextBox5.Location = New System.Drawing.Point(567, 233)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(73, 26)
+        Me.TextBox5.TabIndex = 15
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Enabled = False
+        Me.TextBox6.Location = New System.Drawing.Point(567, 170)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(73, 26)
+        Me.TextBox6.TabIndex = 14
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Enabled = False
+        Me.TextBox7.Location = New System.Drawing.Point(567, 110)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(73, 26)
+        Me.TextBox7.TabIndex = 13
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Enabled = False
+        Me.TextBox8.Location = New System.Drawing.Point(567, 49)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(73, 26)
+        Me.TextBox8.TabIndex = 12
+        '
+        'chkboxWater
+        '
+        Me.chkboxWater.AutoSize = True
+        Me.chkboxWater.Location = New System.Drawing.Point(262, 51)
+        Me.chkboxWater.Name = "chkboxWater"
+        Me.chkboxWater.Size = New System.Drawing.Size(75, 24)
+        Me.chkboxWater.TabIndex = 16
+        Me.chkboxWater.Text = "Order"
+        Me.chkboxWater.UseVisualStyleBackColor = True
+        '
+        'chkboxSnack
+        '
+        Me.chkboxSnack.AutoSize = True
+        Me.chkboxSnack.Location = New System.Drawing.Point(262, 112)
+        Me.chkboxSnack.Name = "chkboxSnack"
+        Me.chkboxSnack.Size = New System.Drawing.Size(75, 24)
+        Me.chkboxSnack.TabIndex = 17
+        Me.chkboxSnack.Text = "Order"
+        Me.chkboxSnack.UseVisualStyleBackColor = True
+        '
+        'chkbox5K
+        '
+        Me.chkbox5K.AutoSize = True
+        Me.chkbox5K.Location = New System.Drawing.Point(262, 170)
+        Me.chkbox5K.Name = "chkbox5K"
+        Me.chkbox5K.Size = New System.Drawing.Size(75, 24)
+        Me.chkbox5K.TabIndex = 18
+        Me.chkbox5K.Text = "Order"
+        Me.chkbox5K.UseVisualStyleBackColor = True
+        '
+        'chkbox10K
+        '
+        Me.chkbox10K.AutoSize = True
+        Me.chkbox10K.Location = New System.Drawing.Point(262, 233)
+        Me.chkbox10K.Name = "chkbox10K"
+        Me.chkbox10K.Size = New System.Drawing.Size(75, 24)
+        Me.chkbox10K.TabIndex = 19
+        Me.chkbox10K.Text = "Order"
+        Me.chkbox10K.UseVisualStyleBackColor = True
+        '
+        'chkboxXShirt
+        '
+        Me.chkboxXShirt.AutoSize = True
+        Me.chkboxXShirt.Location = New System.Drawing.Point(646, 234)
+        Me.chkboxXShirt.Name = "chkboxXShirt"
+        Me.chkboxXShirt.Size = New System.Drawing.Size(75, 24)
+        Me.chkboxXShirt.TabIndex = 23
+        Me.chkboxXShirt.Text = "Order"
+        Me.chkboxXShirt.UseVisualStyleBackColor = True
+        '
+        'chkboxLargeShirt
+        '
+        Me.chkboxLargeShirt.AutoSize = True
+        Me.chkboxLargeShirt.Location = New System.Drawing.Point(646, 171)
+        Me.chkboxLargeShirt.Name = "chkboxLargeShirt"
+        Me.chkboxLargeShirt.Size = New System.Drawing.Size(75, 24)
+        Me.chkboxLargeShirt.TabIndex = 22
+        Me.chkboxLargeShirt.Text = "Order"
+        Me.chkboxLargeShirt.UseVisualStyleBackColor = True
+        '
+        'chkboxMedShirt
+        '
+        Me.chkboxMedShirt.AutoSize = True
+        Me.chkboxMedShirt.Location = New System.Drawing.Point(646, 113)
+        Me.chkboxMedShirt.Name = "chkboxMedShirt"
+        Me.chkboxMedShirt.Size = New System.Drawing.Size(75, 24)
+        Me.chkboxMedShirt.TabIndex = 21
+        Me.chkboxMedShirt.Text = "Order"
+        Me.chkboxMedShirt.UseVisualStyleBackColor = True
+        '
+        'chkboxSmallshirt
+        '
+        Me.chkboxSmallshirt.AutoSize = True
+        Me.chkboxSmallshirt.Location = New System.Drawing.Point(646, 52)
+        Me.chkboxSmallshirt.Name = "chkboxSmallshirt"
+        Me.chkboxSmallshirt.Size = New System.Drawing.Size(75, 24)
+        Me.chkboxSmallshirt.TabIndex = 20
+        Me.chkboxSmallshirt.Text = "Order"
+        Me.chkboxSmallshirt.UseVisualStyleBackColor = True
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(156, 292)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(100, 40)
+        Me.btnSubmit.TabIndex = 24
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(328, 292)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(100, 40)
+        Me.btnClear.TabIndex = 25
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(499, 292)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 40)
+        Me.btnExit.TabIndex = 26
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'ViewInventory
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(802, 363)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.chkboxXShirt)
+        Me.Controls.Add(Me.chkboxLargeShirt)
+        Me.Controls.Add(Me.chkboxMedShirt)
+        Me.Controls.Add(Me.chkboxSmallshirt)
+        Me.Controls.Add(Me.chkbox10K)
+        Me.Controls.Add(Me.chkbox5K)
+        Me.Controls.Add(Me.chkboxSnack)
+        Me.Controls.Add(Me.chkboxWater)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ViewInventory"
+        Me.Text = "View Inventory"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents chkboxWater As CheckBox
+    Friend WithEvents chkboxSnack As CheckBox
+    Friend WithEvents chkbox5K As CheckBox
+    Friend WithEvents chkbox10K As CheckBox
+    Friend WithEvents chkboxXShirt As CheckBox
+    Friend WithEvents chkboxLargeShirt As CheckBox
+    Friend WithEvents chkboxMedShirt As CheckBox
+    Friend WithEvents chkboxSmallshirt As CheckBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ MarathonFinalProject/MarathonFinalProject/ViewInventory.vb
@@ -0,0 +1,9 @@
+﻿Public Class ViewInventory
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+End Class
