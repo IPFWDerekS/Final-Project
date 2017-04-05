5c5fe018a47e489a795a354bad2ac56e83c62c1d
Derek Singson <Derek Singson>
2017-04-05 17:41:21 -0400

    Added controls to the Add Runner Form!

--- MarathonFinalProject/MarathonFinalProject/Form2.Designer.vb
+++ MarathonFinalProject/MarathonFinalProject/Form2.Designer.vb
@@ -26,18 +26,42 @@ Partial Class frmAddRunner
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label10 = New System.Windows.Forms.Label()
         Me.Label11 = New System.Windows.Forms.Label()
         Me.Label12 = New System.Windows.Forms.Label()
+        Me.txtBibnumber = New System.Windows.Forms.TextBox()
+        Me.txtFirst = New System.Windows.Forms.TextBox()
+        Me.txtLast = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.cboShirts = New System.Windows.Forms.ComboBox()
+        Me.grpboxRaceType = New System.Windows.Forms.GroupBox()
+        Me.radbtn5K = New System.Windows.Forms.RadioButton()
+        Me.radbtn10KRace = New System.Windows.Forms.RadioButton()
+        Me.grpboxCorralNumber = New System.Windows.Forms.GroupBox()
+        Me.radbtnCorralA = New System.Windows.Forms.RadioButton()
+        Me.radbtnCorralB = New System.Windows.Forms.RadioButton()
+        Me.radbtnCorralC = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.radbtn5KMedal = New System.Windows.Forms.RadioButton()
+        Me.radbtn10KMedal = New System.Windows.Forms.RadioButton()
+        Me.txtdisplayWater = New System.Windows.Forms.TextBox()
+        Me.txtDisplaySnack = New System.Windows.Forms.TextBox()
+        Me.txtdisplay5K = New System.Windows.Forms.TextBox()
+        Me.txtdisplay10K = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.grpboxRaceType.SuspendLayout()
+        Me.grpboxCorralNumber.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(29, 42)
+        Me.Label1.Location = New System.Drawing.Point(3, 45)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(100, 20)
         Me.Label1.TabIndex = 0
@@ -46,7 +70,7 @@ Partial Class frmAddRunner
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(33, 100)
+        Me.Label2.Location = New System.Drawing.Point(344, 45)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(90, 20)
         Me.Label2.TabIndex = 1
@@ -55,7 +79,7 @@ Partial Class frmAddRunner
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(29, 165)
+        Me.Label3.Location = New System.Drawing.Point(344, 100)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(94, 20)
         Me.Label3.TabIndex = 2
@@ -64,34 +88,16 @@ Partial Class frmAddRunner
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(29, 274)
+        Me.Label4.Location = New System.Drawing.Point(14, 152)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(89, 20)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Shirt Size:  "
         '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(33, 348)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(85, 20)
-        Me.Label7.TabIndex = 7
-        Me.Label7.Text = "Race Type"
-        '
-        'Label8
-        '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(29, 430)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(123, 20)
-        Me.Label8.TabIndex = 8
-        Me.Label8.Text = "Corral Number:  "
-        '
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(29, 505)
+        Me.Label9.Location = New System.Drawing.Point(34, 499)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(106, 20)
         Me.Label9.TabIndex = 9
@@ -100,7 +106,7 @@ Partial Class frmAddRunner
         'Label10
         '
         Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(253, 505)
+        Me.Label10.Location = New System.Drawing.Point(249, 499)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(62, 20)
         Me.Label10.TabIndex = 10
@@ -109,7 +115,7 @@ Partial Class frmAddRunner
         'Label11
         '
         Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(423, 505)
+        Me.Label11.Location = New System.Drawing.Point(419, 499)
         Me.Label11.Name = "Label11"
         Me.Label11.Size = New System.Drawing.Size(83, 20)
         Me.Label11.TabIndex = 11
@@ -118,29 +124,265 @@ Partial Class frmAddRunner
         'Label12
         '
         Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(590, 505)
+        Me.Label12.Location = New System.Drawing.Point(576, 499)
         Me.Label12.Name = "Label12"
         Me.Label12.Size = New System.Drawing.Size(92, 20)
         Me.Label12.TabIndex = 12
         Me.Label12.Text = "10K Medal: "
+        '
+        'txtBibnumber
+        '
+        Me.txtBibnumber.Location = New System.Drawing.Point(118, 42)
+        Me.txtBibnumber.Name = "txtBibnumber"
+        Me.txtBibnumber.Size = New System.Drawing.Size(147, 26)
+        Me.txtBibnumber.TabIndex = 13
+        '
+        'txtFirst
+        '
+        Me.txtFirst.Location = New System.Drawing.Point(475, 39)
+        Me.txtFirst.Name = "txtFirst"
+        Me.txtFirst.Size = New System.Drawing.Size(253, 26)
+        Me.txtFirst.TabIndex = 14
+        '
+        'txtLast
+        '
+        Me.txtLast.Location = New System.Drawing.Point(475, 94)
+        Me.txtLast.Name = "txtLast"
+        Me.txtLast.Size = New System.Drawing.Size(253, 26)
+        Me.txtLast.TabIndex = 15
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(61, 100)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 20)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Age:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(118, 97)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(76, 26)
+        Me.TextBox1.TabIndex = 17
+        '
+        'cboShirts
+        '
+        Me.cboShirts.FormattingEnabled = True
+        Me.cboShirts.Items.AddRange(New Object() {"Small", "Medium", "Large", "X-Large"})
+        Me.cboShirts.Location = New System.Drawing.Point(118, 152)
+        Me.cboShirts.Name = "cboShirts"
+        Me.cboShirts.Size = New System.Drawing.Size(121, 28)
+        Me.cboShirts.TabIndex = 18
+        '
+        'grpboxRaceType
+        '
+        Me.grpboxRaceType.Controls.Add(Me.radbtn10KRace)
+        Me.grpboxRaceType.Controls.Add(Me.radbtn5K)
+        Me.grpboxRaceType.Location = New System.Drawing.Point(18, 249)
+        Me.grpboxRaceType.Name = "grpboxRaceType"
+        Me.grpboxRaceType.Size = New System.Drawing.Size(247, 124)
+        Me.grpboxRaceType.TabIndex = 19
+        Me.grpboxRaceType.TabStop = False
+        Me.grpboxRaceType.Text = "Race Type:"
+        '
+        'radbtn5K
+        '
+        Me.radbtn5K.AutoSize = True
+        Me.radbtn5K.Location = New System.Drawing.Point(47, 36)
+        Me.radbtn5K.Name = "radbtn5K"
+        Me.radbtn5K.Size = New System.Drawing.Size(95, 24)
+        Me.radbtn5K.TabIndex = 0
+        Me.radbtn5K.TabStop = True
+        Me.radbtn5K.Text = "5K Race"
+        Me.radbtn5K.UseVisualStyleBackColor = True
+        '
+        'radbtn10KRace
+        '
+        Me.radbtn10KRace.AutoSize = True
+        Me.radbtn10KRace.Location = New System.Drawing.Point(47, 82)
+        Me.radbtn10KRace.Name = "radbtn10KRace"
+        Me.radbtn10KRace.Size = New System.Drawing.Size(104, 24)
+        Me.radbtn10KRace.TabIndex = 1
+        Me.radbtn10KRace.TabStop = True
+        Me.radbtn10KRace.Text = "10K Race"
+        Me.radbtn10KRace.UseVisualStyleBackColor = True
+        '
+        'grpboxCorralNumber
+        '
+        Me.grpboxCorralNumber.Controls.Add(Me.radbtnCorralC)
+        Me.grpboxCorralNumber.Controls.Add(Me.radbtnCorralB)
+        Me.grpboxCorralNumber.Controls.Add(Me.radbtnCorralA)
+        Me.grpboxCorralNumber.Location = New System.Drawing.Point(348, 249)
+        Me.grpboxCorralNumber.Name = "grpboxCorralNumber"
+        Me.grpboxCorralNumber.Size = New System.Drawing.Size(184, 124)
+        Me.grpboxCorralNumber.TabIndex = 20
+        Me.grpboxCorralNumber.TabStop = False
+        Me.grpboxCorralNumber.Text = "Corral Number:"
+        '
+        'radbtnCorralA
+        '
+        Me.radbtnCorralA.AutoSize = True
+        Me.radbtnCorralA.Location = New System.Drawing.Point(23, 26)
+        Me.radbtnCorralA.Name = "radbtnCorralA"
+        Me.radbtnCorralA.Size = New System.Drawing.Size(91, 24)
+        Me.radbtnCorralA.TabIndex = 0
+        Me.radbtnCorralA.TabStop = True
+        Me.radbtnCorralA.Text = "Corral A"
+        Me.radbtnCorralA.UseVisualStyleBackColor = True
+        '
+        'radbtnCorralB
+        '
+        Me.radbtnCorralB.AutoSize = True
+        Me.radbtnCorralB.Location = New System.Drawing.Point(23, 56)
+        Me.radbtnCorralB.Name = "radbtnCorralB"
+        Me.radbtnCorralB.Size = New System.Drawing.Size(91, 24)
+        Me.radbtnCorralB.TabIndex = 1
+        Me.radbtnCorralB.TabStop = True
+        Me.radbtnCorralB.Text = "Corral B"
+        Me.radbtnCorralB.UseVisualStyleBackColor = True
+        '
+        'radbtnCorralC
+        '
+        Me.radbtnCorralC.AutoSize = True
+        Me.radbtnCorralC.Location = New System.Drawing.Point(23, 94)
+        Me.radbtnCorralC.Name = "radbtnCorralC"
+        Me.radbtnCorralC.Size = New System.Drawing.Size(91, 24)
+        Me.radbtnCorralC.TabIndex = 2
+        Me.radbtnCorralC.TabStop = True
+        Me.radbtnCorralC.Text = "Corral C"
+        Me.radbtnCorralC.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radbtn10KMedal)
+        Me.GroupBox1.Controls.Add(Me.radbtn5KMedal)
+        Me.GroupBox1.Location = New System.Drawing.Point(567, 255)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 118)
+        Me.GroupBox1.TabIndex = 21
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "grpboxMedals"
+        '
+        'radbtn5KMedal
+        '
+        Me.radbtn5KMedal.AutoSize = True
+        Me.radbtn5KMedal.Location = New System.Drawing.Point(27, 26)
+        Me.radbtn5KMedal.Name = "radbtn5KMedal"
+        Me.radbtn5KMedal.Size = New System.Drawing.Size(100, 24)
+        Me.radbtn5KMedal.TabIndex = 0
+        Me.radbtn5KMedal.TabStop = True
+        Me.radbtn5KMedal.Text = "5K Medal"
+        Me.radbtn5KMedal.UseVisualStyleBackColor = True
+        '
+        'radbtn10KMedal
+        '
+        Me.radbtn10KMedal.AutoSize = True
+        Me.radbtn10KMedal.Location = New System.Drawing.Point(28, 76)
+        Me.radbtn10KMedal.Name = "radbtn10KMedal"
+        Me.radbtn10KMedal.Size = New System.Drawing.Size(109, 24)
+        Me.radbtn10KMedal.TabIndex = 1
+        Me.radbtn10KMedal.TabStop = True
+        Me.radbtn10KMedal.Text = "10K Medal"
+        Me.radbtn10KMedal.UseVisualStyleBackColor = True
+        '
+        'txtdisplayWater
+        '
+        Me.txtdisplayWater.Enabled = False
+        Me.txtdisplayWater.Location = New System.Drawing.Point(146, 496)
+        Me.txtdisplayWater.Name = "txtdisplayWater"
+        Me.txtdisplayWater.Size = New System.Drawing.Size(48, 26)
+        Me.txtdisplayWater.TabIndex = 22
+        '
+        'txtDisplaySnack
+        '
+        Me.txtDisplaySnack.Enabled = False
+        Me.txtDisplaySnack.Location = New System.Drawing.Point(317, 496)
+        Me.txtDisplaySnack.Name = "txtDisplaySnack"
+        Me.txtDisplaySnack.Size = New System.Drawing.Size(48, 26)
+        Me.txtDisplaySnack.TabIndex = 23
+        '
+        'txtdisplay5K
+        '
+        Me.txtdisplay5K.Enabled = False
+        Me.txtdisplay5K.Location = New System.Drawing.Point(508, 496)
+        Me.txtdisplay5K.Name = "txtdisplay5K"
+        Me.txtdisplay5K.Size = New System.Drawing.Size(48, 26)
+        Me.txtdisplay5K.TabIndex = 24
+        '
+        'txtdisplay10K
+        '
+        Me.txtdisplay10K.Enabled = False
+        Me.txtdisplay10K.Location = New System.Drawing.Point(674, 496)
+        Me.txtdisplay10K.Name = "txtdisplay10K"
+        Me.txtdisplay10K.Size = New System.Drawing.Size(48, 26)
+        Me.txtdisplay10K.TabIndex = 25
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(119, 585)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 33)
+        Me.btnSubmit.TabIndex = 26
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(290, 585)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(96, 33)
+        Me.btnClear.TabIndex = 27
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(517, 585)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(86, 33)
+        Me.btnExit.TabIndex = 28
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
         'frmAddRunner
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(779, 684)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtdisplay10K)
+        Me.Controls.Add(Me.txtdisplay5K)
+        Me.Controls.Add(Me.txtDisplaySnack)
+        Me.Controls.Add(Me.txtdisplayWater)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.grpboxCorralNumber)
+        Me.Controls.Add(Me.grpboxRaceType)
+        Me.Controls.Add(Me.cboShirts)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtLast)
+        Me.Controls.Add(Me.txtFirst)
+        Me.Controls.Add(Me.txtBibnumber)
         Me.Controls.Add(Me.Label12)
         Me.Controls.Add(Me.Label11)
         Me.Controls.Add(Me.Label10)
         Me.Controls.Add(Me.Label9)
-        Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "frmAddRunner"
         Me.Text = "Add Runner"
+        Me.grpboxRaceType.ResumeLayout(False)
+        Me.grpboxRaceType.PerformLayout()
+        Me.grpboxCorralNumber.ResumeLayout(False)
+        Me.grpboxCorralNumber.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -150,10 +392,31 @@ Partial Class frmAddRunner
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents Label7 As Label
-    Friend WithEvents Label8 As Label
     Friend WithEvents Label9 As Label
     Friend WithEvents Label10 As Label
     Friend WithEvents Label11 As Label
     Friend WithEvents Label12 As Label
+    Friend WithEvents txtBibnumber As TextBox
+    Friend WithEvents txtFirst As TextBox
+    Friend WithEvents txtLast As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents cboShirts As ComboBox
+    Friend WithEvents grpboxRaceType As GroupBox
+    Friend WithEvents radbtn10KRace As RadioButton
+    Friend WithEvents radbtn5K As RadioButton
+    Friend WithEvents grpboxCorralNumber As GroupBox
+    Friend WithEvents radbtnCorralC As RadioButton
+    Friend WithEvents radbtnCorralB As RadioButton
+    Friend WithEvents radbtnCorralA As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents radbtn10KMedal As RadioButton
+    Friend WithEvents radbtn5KMedal As RadioButton
+    Friend WithEvents txtdisplayWater As TextBox
+    Friend WithEvents txtDisplaySnack As TextBox
+    Friend WithEvents txtdisplay5K As TextBox
+    Friend WithEvents txtdisplay10K As TextBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
 End Class
--- MarathonFinalProject/MarathonFinalProject/Form2.vb
+++ MarathonFinalProject/MarathonFinalProject/Form2.vb
@@ -2,4 +2,8 @@
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
 
     End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles grpboxRaceType.Enter
+
+    End Sub
 End Class
