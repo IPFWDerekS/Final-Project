710ea34a37e2be4a423494528846aca69d7bec70
Derek Singson <Derek Singson>
2017-04-05 15:19:04 -0400

    For the Main Menu Form I added the Menu Strip.

--- MarathonFinalProject/MarathonFinalProject/Form1.Designer.vb
+++ MarathonFinalProject/MarathonFinalProject/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMainForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,17 +22,76 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.toolstrpMenu = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menustrpbtnAddRunner = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuitemRunnerForm = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menustrpViewInventory = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menustrpExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Form1
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.toolstrpMenu})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(791, 33)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'toolstrpMenu
+        '
+        Me.toolstrpMenu.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menustrpbtnAddRunner, Me.menuitemRunnerForm, Me.menustrpViewInventory, Me.menustrpExit})
+        Me.toolstrpMenu.Name = "toolstrpMenu"
+        Me.toolstrpMenu.Size = New System.Drawing.Size(69, 29)
+        Me.toolstrpMenu.Text = "Menu"
+        '
+        'menustrpbtnAddRunner
+        '
+        Me.menustrpbtnAddRunner.Name = "menustrpbtnAddRunner"
+        Me.menustrpbtnAddRunner.Size = New System.Drawing.Size(214, 30)
+        Me.menustrpbtnAddRunner.Text = "Add Runner"
+        '
+        'menuitemRunnerForm
+        '
+        Me.menuitemRunnerForm.Name = "menuitemRunnerForm"
+        Me.menuitemRunnerForm.Size = New System.Drawing.Size(214, 30)
+        Me.menuitemRunnerForm.Text = "Runner Form"
+        '
+        'menustrpViewInventory
+        '
+        Me.menustrpViewInventory.Name = "menustrpViewInventory"
+        Me.menustrpViewInventory.Size = New System.Drawing.Size(214, 30)
+        Me.menustrpViewInventory.Text = "View Inventory"
+        '
+        'menustrpExit
+        '
+        Me.menustrpExit.Name = "menustrpExit"
+        Me.menustrpExit.Size = New System.Drawing.Size(214, 30)
+        Me.menustrpExit.Text = "Exit"
+        '
+        'frmMainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(356, 321)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(791, 469)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmMainForm"
+        Me.Text = "Main Menu"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents toolstrpMenu As ToolStripMenuItem
+    Friend WithEvents menustrpbtnAddRunner As ToolStripMenuItem
+    Friend WithEvents menuitemRunnerForm As ToolStripMenuItem
+    Friend WithEvents menustrpViewInventory As ToolStripMenuItem
+    Friend WithEvents menustrpExit As ToolStripMenuItem
 End Class
--- MarathonFinalProject/MarathonFinalProject/Form1.vb
+++ MarathonFinalProject/MarathonFinalProject/Form1.vb
@@ -1,5 +1,26 @@
-﻿Public Class Form1
+﻿Public Class frmMainForm
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'The main form the user will use is 
+        'a Form that has 3 buttons Or 
+        '3 menustrip buttons to open 3 other forms.
+        'So there are 4 forms in total. 
+        'One of the three Is the 'Add runner’ 
+        'Form, this form will have:  3 Textboxes; f
+        'Or Name, Age, BIB Number. 1 combobox for 
+        'the shirt sizes: small, medium , large, 
+        'extra large. 5 radio Button for either5K '
+        'Or 10K, then the rest for the Corral A, B, C.
+        '4 Check boxes for the inventory: water bottle, snacks,
+        '5K Medal, 10K Medal. 3 Button called; 'Submit’, 
+        'Clear’, and ‘Exit’. A second will ‘Runners Form’ will 
+        'have a data grid view of the Runners. It'll enable the
+        'user to search and select runners. Fourth form is 'View Inventory’, 
+        'where the user will see the amount Of the water, snacks, medals, And shirts.
+
+
+    End Sub
+
+    Private Sub VToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles menuitemRunnerForm.Click
 
     End Sub
 End Class
