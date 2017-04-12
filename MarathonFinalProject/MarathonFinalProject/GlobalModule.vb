9e01fa781afd13b3d07e22c032274381ede2e3e4
Derek Singson <Derek Singson>
2017-04-12 16:23:26 -0400

    1sts requirement progress: Creating the variables for the items count

--- MarathonFinalProject/MarathonFinalProject/GlobalModule.vb
+++ MarathonFinalProject/MarathonFinalProject/GlobalModule.vb
@@ -1,3 +1,23 @@
 ﻿Module GlobalModule
 
+    'The inventory count
+    Dim smallShirt As Integer
+
+    Dim mediumShirt As Integer
+
+    Dim largeShirt As Integer
+
+    Dim xlargeShirt As Integer
+
+    Dim waterBottle As Integer
+
+    Dim snack As Integer
+
+    Dim fiveKMedal As Integer
+
+    Dim tenKMedal As Integer
+
+
+
+
 End Module
